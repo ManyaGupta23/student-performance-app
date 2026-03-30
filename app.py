1ee75125ce48a21cefbd1fe902dc1f64010d39da
Manya Gupta <manyagupta201@gmail.com>
2026-03-30 15:43:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ else:
     df = new_data
     df.to_excel(file_name, index=False)
     with st.spinner("⏳ Saving..."):
-    time.sleep(1)
-    st.success("🎉 Data Saved Successfully!")
+        time.sleep(1)
+         st.success("🎉 Data Saved Successfully!")
 
 # ---------- DASHBOARD ----------
 if page == "Dashboard":
