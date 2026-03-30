d6a9ca3701f62cccbb7fe1c817b5783ee0ea7455
Manya Gupta <manyagupta201@gmail.com>
2026-03-30 15:39:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,24 +85,18 @@ if page == "Data Entry":
                 "gpa":[GPA]
             })
 file_name="student_data.xlsx"
-            if os.path.exists(file_name):
-                df = pd.read_excel(file_name)
-
-                df.columns = df.columns.str.strip().str.lower()
-                new_data.columns = new_data.columns.str.strip().str.lower()
-
-                new_data = new_data.reindex(columns=df.columns, fill_value=0)
-
-                df = pd.concat([df, new_data], ignore_index=True)
-            else:
-                df = new_data
-
-            df.to_excel(file_name, index=False)
-
-            with st.spinner("⏳ Saving..."):
-                time.sleep(1)
-
-            st.success("🎉 Data Saved Successfully!")
+if os.path.exists(file_name):
+    df = pd.read_excel(file_name)
+    df.columns = df.columns.str.strip().str.lower()
+        new_data.columns = new_data.columns.str.strip().str.lower()
+        new_data = new_data.reindex(columns=df.columns, fill_value=0)
+        df = pd.concat([df, new_data], ignore_index=True)
+else:
+    df = new_data
+    df.to_excel(file_name, index=False)
+    with st.spinner("⏳ Saving..."):
+         time.sleep(1)
+     st.success("🎉 Data Saved Successfully!")
 
 # ---------- DASHBOARD ----------
 if page == "Dashboard":
